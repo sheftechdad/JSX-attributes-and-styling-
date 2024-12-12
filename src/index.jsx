6b7e18d04d36ff094657ed07a7508b7c9be5135a
sheftechdad <shefalichaurasia30945@gmail.com>
2024-12-12 22:03:09 +0530

    trying jsx styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = " https://picsum.photos/200/300;"
+
 ReactDOM.render(
   <div>
     <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <img src="https://media.istockphoto.com/id/1975701807/photo/bucatini-pasta-with-tomato-sauce-and-basil-leaves.webp?a=1&b=1&s=612x612&w=0&k=20&c=PQQtlgTkiDpbW6e-_3lLGttJbnAAqLzVEGcgNbxDEC4=" alt="random" />
+    <div>
+      <p className="fun">Red Sauce Pasta</p>
+    </div>
   </div>,
+ 
   document.getElementById("root")
 );
 
