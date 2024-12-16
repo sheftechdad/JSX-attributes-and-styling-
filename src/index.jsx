3063e3a169279eb1fecc4c3e65dd40f1545f5790
sheftechdad <shefalichaurasia30945@gmail.com>
2024-12-16 23:20:11 +0530

    add the photo

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ ReactDOM.render(
     <h1>My Favourite Foods</h1>
     <img src="https://media.istockphoto.com/id/1975701807/photo/bucatini-pasta-with-tomato-sauce-and-basil-leaves.webp?a=1&b=1&s=612x612&w=0&k=20&c=PQQtlgTkiDpbW6e-_3lLGttJbnAAqLzVEGcgNbxDEC4=" alt="random" />
     <div>
-      <p className="fun">Red Sauce Pasta</p>
+      <p className="fun">Red Sauce Pasta </p>
     </div>
   </div>,
  
